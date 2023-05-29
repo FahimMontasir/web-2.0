5cf84742de943321a8e3c1fb53777702d0895f02
FahimMontasir <fahimmontasirshakil@gmail.com>
2023-05-29 16:26:37 +0600

    [done] setup husky and lint-staged

--- 3. varsity management project/src/app.ts
+++ 3. varsity management project/src/app.ts
@@ -2,15 +2,15 @@ import express, { Application } from 'express';
 import cors from 'cors';
 const app: Application = express();
 
-        app.use(    cors());
+app.use(cors());
 
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //testing
-app.get('/',     (req, res) => {
+app.get('/', (req, res) => {
   res.send('working');
 });
 
-export default             app;
+export default app;
