33b49aad0c4bbaa809af6d91430dffa8ba7ebef8
FahimMontasir <fahimmontasirshakil@gmail.com>
2023-05-29 15:55:35 +0600

    testing husky

--- 3. varsity management project/src/app.ts
+++ 3. varsity management project/src/app.ts
@@ -2,14 +2,14 @@ import express, { Application } from 'express';
 import cors from 'cors';
 const app: Application = express();
 
-            app.use(    cors());
+        app.use(    cors());
 
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //testing
-app.get('/', (req, res) => {
+app.get('/',     (req, res) => {
   res.send('working');
 });
 
