d94b091c61a02d8e657f8f7ed9e0920664d831af
FahimMontasir <fahimmontasirshakil@gmail.com>
2023-08-05 16:23:39 +0600

    [add] next-auth

---
+++ 5.nextJs/next-auth/src/pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { useSession } from "next-auth/react";
+const HomePage = () => {
+  const { data: session } = useSession();
+  return (
+    <div>
+      <Head>
+        <title>Next Auth</title>
+      </Head>
+      <h1 style={{ textAlign: "center", marginTop: "10%" }}>
+        Welcome To Next Auth Home Page
+      </h1>
+      <h2>Logged in user: {session?.user.name}</h2>
+    </div>
+  );
+};
+
+export default HomePage;
