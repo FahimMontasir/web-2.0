a79e937449d8dd8aef34b255c60042f8fed0d1f2
FahimMontasir <fahimmontasirshakil@gmail.com>
2023-09-21 15:27:42 +0600

    [add] academic s. prisma

--- 3.core-service/src/app/routes/index.ts
+++ 3.core-service/src/app/routes/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import { AcademicSemeterRoutes } from '../modules/academicSemester/academicSemester.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
   // ... routes
   {
-    path: "",
-    routes: ""
-  }
+    path: '/academic-semesters',
+    route: AcademicSemeterRoutes,
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
